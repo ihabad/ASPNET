b8a54785ba11d4c777b71af36d3c611176edba98
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-09-17 12:22:08 -0700

    start project

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using Org.BouncyCastle.Crypto.Prng.Drbg;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
